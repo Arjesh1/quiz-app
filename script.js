368e03733badc543ab8811ce771e52afc9f292a6
Arjesh Khadka <109441944+Arjesh1@users.noreply.github.com>
2023-03-11 22:08:16 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+const quizData = [
+    {
+
+        question: "What is the full form of HTML?",
+        a: "HyperText Markup Language",
+        b:"HyperText Mark Language",
+        c:"HyperText up Language",
+        d:"Hyper Mark Language",
+        correct:"a"
+    },
+
+    {
+
+        question: "What is the full form of HTML?",
+        a: "HyperText Markup Language",
+        b:"HyperText Mark Language",
+        c:"HyperText up Language",
+        d:"Hyper Mark Language",
+        correct:"a"
+    },
+
+    {
+
+        question: "What is the full form of HTML?",
+        a: "HyperText Markup Language",
+        b:"HyperText Mark Language",
+        c:"HyperText up Language",
+        d:"Hyper Mark Language",
+        correct:"a"
+    },
+]
+
+const quiz = document.getElementById('quiz') 
+const answerEls = document.querySelectorAll('.answer')
+const a_text = document.getElementById('a-text')
+const b_text = document.getElementById('b-text')
+const c_text = document.getElementById('c-text')
+const d_text = document.getElementById('d-text')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+function loadQuiz() {
+    de
+}
